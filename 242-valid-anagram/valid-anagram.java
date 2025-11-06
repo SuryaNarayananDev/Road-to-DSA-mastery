bc1455670b88e864c52cb9660eef390f9f8e616b
Surya Narayanan S <144770284+SuryaNarayananDev@users.noreply.github.com>
2025-11-06 18:50:58 +0530

    Time: 5 ms (55.64%) | Memory: 44.5 MB (53.72%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,11 +3,13 @@ class Solution {
         if(s.length()!=t.length()) return false;
  
         int[] arr1=new int[26];
-        for(int i=0;i<s.length();i++){
+        int i=0;
+        while(i<s.length()){
             arr1[s.charAt(i)-'a']++;
             arr1[t.charAt(i)-'a']--;
+            i++;
         }
-        for(int i=0;i<26;i++){
+        for(i=0;i<26;i++){
             if(arr1[i]!=0) return false;
         }
         return true;
